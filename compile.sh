530ba5fb847c83d55dba8f9ef11e0dc22d2b7f7c
XUANXUQAQ <xuanxusb@outlook.com>
2025-12-29 20:33:03 +0800

    Add compile script

---
+++ compile.sh
@@ -0,0 +1,7 @@
+ZT_VERSION=1.16.0
+proxychains wget https://github.com/zerotier/ZeroTierOne/archive/refs/tags/$ZT_VERSION.tar.gz
+tar zxf $ZT_VERSION.tar.gz
+cd ZeroTierOne-$ZT_VERSION
+make -j`nproc` ZT_STATIC=1
+strip zerotier-one
+tar -czvf zerotier_$(uname -p)_$ZT_VERSION.tar.gz zerotier-one zerotier-cli
